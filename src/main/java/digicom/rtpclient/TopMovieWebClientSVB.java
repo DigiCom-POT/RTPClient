3d3912161a6503ea0ccb97e36aa68ccceb600155
sagarprasad <sagar.prasad@gmail.com>
2014-12-12 13:39:21 +0530

    adding comment for APIs

--- src/main/java/digicom/rtpclient/TopMovieWebClientSVB.java
+++ src/main/java/digicom/rtpclient/TopMovieWebClientSVB.java
@@ -25,22 +25,43 @@ public class TopMovieWebClientSVB {
 		return g.toJson(cassandraConnector.getTopMovies(mode));
 	}
 
+	/**
+	 * Gives the result from real time processed data
+	 * (Spark Streaming)
+	 * @return
+	 */
 	@RequestMapping("/recommendations/TopMovies")
 	public String getSortedTopMovies() {
 		CassandraConnector cassandraConnector = new CassandraConnector();
 		return cassandraConnector.getTopMoviesList().toString();
 	}
 
+	/**
+	 * Recommends the movies based on user taste.
+	 * Use User-Item based CF
+	 * @param userID
+	 * @return
+	 */
 	@RequestMapping("/recommendations/UserBased")
 	public String userRecommendMovie(@RequestParam String userID) {
 		return FluentHbaseClient.userRecommendMovie(userID);
 	}
 
+	/**
+	 * Recommends the movie based on Item-Item Similarity
+	 * It is more static in nature
+	 * @param movieID
+	 * @return
+	 */
 	@RequestMapping("/recommendations/ItemBased")
 	public String similarMovieRecommend(@RequestParam String movieID) {
 		return FluentHbaseClient.similarMovieRecommend(movieID);
 	}
 
+	/**
+	 * Gives the result from real time and batch processed data
+	 * @return
+	 */
 	@RequestMapping("/recommendations/AllTimeTopMovies")
 	public String allTimeTopMovies() {
 		CassandraConnector cassandraConnector = new CassandraConnector();
